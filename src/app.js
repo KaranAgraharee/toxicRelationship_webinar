6d765bb2ba56218fc79e06907998671048111ae8
Karan Agrahari <114682523+KaranAgraharee@users.noreply.github.com>
2026-06-23 18:44:12 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -9,12 +9,7 @@ import { env } from "./config/env.js";
 
 const app = express();
 
-app.use(cors(
-  {
-    origin: 'https://webinar.khushnay.com',
-    credentials: true,
-  }
-));
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
